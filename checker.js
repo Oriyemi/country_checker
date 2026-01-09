46654899a675c127b205367cece4e0ee85fd4afc
Faith Esan <oriyemiesan@gmail.com>
2026-01-09 21:42:33 +0100

    fix typo error

--- checker.js
+++ checker.js
@@ -4,14 +4,14 @@ function regionHandler() {
 
     let country = prompt("state the country").trim().toLowerCase()
     switch (true) {
-        case country == "gambia" || country == "mali" || country == "benin" || country == "burkina faso" || country == "cape verde" || country == "ghana" || country == "guinea" || country == "guinea bissau" || country == "ivory coast" || country == "Côte d’lvoire" || country == "liberia" || country == "mauritania" || country == "niger" || country == "nigeria" || country == "senegal" || country == "sierra leaone" || country == "togo":
+        case country == "gambia" || country == "mali" || country == "benin" || country == "burkina faso" || country == "cape verde" || country == "ghana" || country == "guinea" || country == "guinea bissau" || country == "ivory coast" || country == "côte d’lvoire" || country == "liberia" || country == "mauritania" || country == "niger" || country == "nigeria" || country == "senegal" || country == "sierra leone" || country == "togo":
             alert("West Africa")
             break;
         
         case country == "algeria" || country == "egypt" || country == "libya" || country == "sudan" || country == "tunisia" || country == "morocco":
             alert("North Africa")
             break;
-        case country == "south africa" || country == "namibia" || country == "botswana" || country == "zimbabwe" || country == "zambia" || country == "mozanbique" || country == "lesotho" || country == "eswatini" || country == "swaziland" || country == "angola" || country == "malawi":
+        case country == "south africa" || country == "namibia" || country == "botswana" || country == "zimbabwe" || country == "zambia" || country == "mozambique" || country == "lesotho" || country == "eswatini" || country == "swaziland" || country == "angola" || country == "malawi":
          alert("South Africa")
             break;
         
@@ -19,8 +19,8 @@ function regionHandler() {
             alert("East Africa")
             break;
         
-        case country == "cameroon" || country == "chad" || country == "gabon" || country == "sao tome" || country == "equatorial guinea" || country == "DR CONGO "|| country == "CONGO" ||country==  "DEMOCRATIC REPUBLIC OF CONGO" ||country=="central african republic":
-            alert("Central Africa")
+        case country == "cameroon" || country == "chad" || country == "gabon" || country == "sao tome" || country == "equatorial guinea" || country == "dr congo"|| country == "congo" ||country==  "democratic republic of congo" ||country=="central african republic":
+            alert(`${country } is in Central Africa`)
             break;
         default:
             alert("This is not an African country")
